1b15657e955ae13b2f10ad2dcee2476c0ed114be
Onkar Deshpande <62983112+onkar964@users.noreply.github.com>
2024-10-22 12:13:39 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -24,14 +24,14 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Onkar Deshpande",
+  title: "Full-stack Developer",
+  email: "onkar964@gmail.com",
+  gitHub: "onkar964",
+  instagram: "onkar964",
+  linkedIn: "Onkar-Deshpande",
   medium: "",
-  twitter: "microsoft",
+  twitter: "onkar964",
   youTube: "Code",
 };
 
