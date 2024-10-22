3a38f36cc5c1d4aaedb848fa0a13144e4b0310cd
Onkar Deshpande <62983112+onkar964@users.noreply.github.com>
2024-10-22 12:11:16 +0530

    Update About.jsx

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -18,25 +18,24 @@ import React from "react";
  */
 import image from "../images/motion-background.jpg";
 
-const imageAltText = "purple and blue abstract background";
+const imageAltText = "background image";
 
 /**
  * Sort description that expands on your title on the Home component.
  */
-const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+const description = "Dynamic and adaptable Full Stack Developer with a solid foundation in SAP SD with 2+ years of experience and a keen interest in transitioning into full-stack web development roles.";
 
 /**
  * List of some of skills or technologies you work on, are learning,
  * passionate about, or enjoy,
  */
 const skillsList = [
-  "Web design",
-  "User experience",
-  "Inclusive design",
-  "Focus group testing",
-  "Mobile user interfaces",
-  "Graphic design",
+  "Web Development",
+  "Front-end Development",
+  "Back-end Development",
+  "Data Science",
+  "Machine Learning",
+  "SAP SD Consultant",
 ];
 
 /**
@@ -45,7 +44,7 @@ const skillsList = [
  * about you on a professional level.
  */
 const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my SAP SD experience I continually look for new and better ways to make tech accessible by all.";
 
 const About = () => {
   return (
